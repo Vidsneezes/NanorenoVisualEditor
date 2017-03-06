20d8af866845b3371b14459d5a8c24f38f035f14
Vidsneezes <moraleszez@gmail.com>
2017-03-06 07:20:06 -0600

    first commit

---
+++ src/components/chaptersview/ChaptersView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ChaptersView extends Component {
+  render() {
+    return (
+      <div className="ChaptersView">
+        <h2>Welcome to React</h2>
+        <div className="ChaptersView-Container">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
