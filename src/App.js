3da1b9a7fd955f74ff69407f9f94186fd16cf328
Vidsneezes <moraleszez@gmail.com>
2017-03-07 05:52:17 -0600

    added cutdata class

--- src/App.js
+++ src/App.js
@@ -3,52 +3,22 @@ import ChaptersView from './components/chaptersview/ChaptersView.js';
 import CutView from './components/cutview/CutView.js';
 import CutBlockEditor from './components/cutview/cutblockeditor/CutBlockEditor.js'
 import './App.css';
-
-const sampleJson = [
-  {
-    "skit": "SchoolgirlA",
-    "emotion": "sad",
-    "text": "Dialog number one",
-    "key":"dfk",
-    "side":"right"
-  },
-  {
-    "skit": "SchoolgirlA",
-    "emotion": "happy",
-    "text": "Dialog number two",
-    "key":"fd",
-    "side":"left"
-  },
-  {
-    "skit": "SchoolgirlB",
-    "emotion": "worry",
-    "text": "Dialog number three",
-    "key":"eee",
-    "side":"right"
-  },
-  {
-    "skit": "SchoolgirlB",
-    "emotion": "punchline",
-    "text": "punchline",
-    "key":"cc",
-    "side":"left"
-  }
-];
+import DataJson from './CutData.js'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      cutData: sampleJson
+      cutData: DataJson.data
     }
     this.handleData = this.handleData.bind(this);
   }
 
   handleData() {
-    sampleJson[0].side = "left";
-    console.log(sampleJson);
+    DataJson.data[0].side = "left";
+    console.log(DataJson.data);
     this.setState({
-      cutData: sampleJson
+      cutData: DataJson.data
     });
   }
 
---
+++ src/CutData.js
@@ -0,0 +1,36 @@
+class DataJson {
+  
+}
+
+DataJson.data = [
+  {
+    "skit": "SchoolgirlA",
+    "emotion": "sad",
+    "text": "Dialog number one",
+    "key":"dfk",
+    "side":"right"
+  },
+  {
+    "skit": "SchoolgirlA",
+    "emotion": "happy",
+    "text": "Dialog number two",
+    "key":"fd",
+    "side":"left"
+  },
+  {
+    "skit": "SchoolgirlB",
+    "emotion": "worry",
+    "text": "Dialog number three",
+    "key":"eee",
+    "side":"right"
+  },
+  {
+    "skit": "SchoolgirlB",
+    "emotion": "punchline",
+    "text": "punchline",
+    "key":"cc",
+    "side":"left"
+  }
+];
+
+export default DataJson;
